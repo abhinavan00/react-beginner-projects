6709857b96b1edbc2cc53677f858d0e20eafaac2
Abhinav Anand <abhinavanand072@gmail.com>
2025-11-13 05:57:03 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './App.css'
+import Accordion from './components/accordion'
+
+function App() {
+
+  return (
+    <>
+      {/* Accordion Component */}
+      <Accordion />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/accordion/data.js
@@ -0,0 +1,16 @@
+const accordionData = [
+  {
+    title: "Section 1 Header",
+    content: "This is the random content for the first section. It can be a paragraph, an image, or other UI elements."
+  },
+  {
+    title: "Section 2 Header",
+    content: "More placeholder information goes here to demonstrate the collapse functionality."
+  },
+  {
+    title: "Section 3 Header",
+    content: "Ensuring all sections can handle dynamic content of varying lengths is important for testing."
+  }
+];
+
+export default accordionData;
