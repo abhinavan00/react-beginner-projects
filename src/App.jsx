a0594307c3ede5a810efd62048a6f4762bcd449f
Abhinav Anand <abhinavanand072@gmail.com>
2025-11-14 05:55:05 +0530

    accordion update

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
 import React from 'react'
 import './App.css'
 import Accordion from './components/accordion'
-import Practice from './components/practice/index_01'
 
 function App() {
 
   return (
     <>
       {/* Accordion Component */}
-      <Practice />
+      <Accordion />
     </>
   )
 }
